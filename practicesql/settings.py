b8d379573195eb913c591faf1e406fa002a31056
Himanshu <hitpatel30112004@gmail.com>
2025-11-07 13:37:23 +0530

    Added CORS headers setup

--- practicesql/settings.py
+++ practicesql/settings.py
@@ -141,7 +141,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://newnetflixfrontend.onrender.com",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
